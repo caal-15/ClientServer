46d211e33aae010d128cc331d310a5e954265e81
Carlos <carlos@CaalDesktop.Carlos>
2014-08-08 17:30:38 -0500

    working threaded Fibo

--- Fib.cpp
+++ Fib.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 #include<thread>
+#include<chrono>
 using namespace std;
 
-void fibo(int n, int *v){
-    *v = n*n;
+int fibo(int n, int *v){
+    if(n <= 2 && n > 0){
+	    return *v = 1;
+	    cout << v << endl;
+    }
+    else{
+        return *v = fibo(n-1, v) + fibo(n-2, v);
+    }
+
 }
 
 int fiboCon(int n){
@@ -16,8 +24,25 @@ int fiboCon(int n){
     }
 
 int main(){
-    int ans;
-    ans = fiboCon(10);
-    cout << ans << endl;
+    int n, ans;
+    chrono::time_point<chrono::system_clock> start, end;
+    chrono::duration<double> elapsed; 
+    cout << "Ingrese el número para calcular su valor en la sucesión de Fibonacci: ";
+    cin >> n;
+    
+    start = chrono::system_clock::now();
+    fibo(n, &ans);
+    end = chrono::system_clock::now();
+    elapsed = end - start;
+    cout << "El Fibonnacci de " << n << " es " << ans << " (Calculo secuencial)" << endl;
+    cout << "El tiempo de ejecucion fue de " << elapsed.count() << " segundos" << endl << endl;
+    
+    start = chrono::system_clock::now(); 
+    ans = fiboCon(n);
+    end = chrono::system_clock::now();
+    elapsed = end - start;
+    cout << "El Fibonnacci de " << n << " es " << ans << " (Calculo Concurrente)" << endl;
+    cout << "El tiempo de ejecucion fue de " << elapsed.count() << " segundos" << endl;
+    
     return 0;
 }

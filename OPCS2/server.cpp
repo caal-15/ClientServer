d244d508089c277f22d296ef83e9a475f4afe46e
Carlos <carlos@CaalLaptop.lol>
2014-09-11 13:00:48 -0500

    Segmentation Fault

--- OPCS2/server.cpp
+++ OPCS2/server.cpp
@@ -1,12 +1,86 @@
-#include <iostream>
-#include <set>
-#include <string>
+#include <bits/stdc++.h>
 #include <czmq.h>
-#include <string.h>
+
 
 using namespace std;
 
-int main(void) {
+unordered_map<string, vector<zframe_t*>> workers;
 
-  return 0;
+void addWorker(zframe_t* id, zmsg_t* incmsg, zmsg_t* outmsg){
+	char* s = zmsg_popstr(incmsg);
+	string op = s;
+	free(s);
+	workers[op].push_back(zframe_dup(id));
+	zmsg_append(outmsg, &id);
+	zmsg_addstr(outmsg, "Thanks");
+	
+}
+
+void dispatchWorker(zmsg_t* incmsg, zmsg_t* outmsg, void* client, void* server){
+	zframe_t* id = zmsg_pop(incmsg);
+	char* s = zmsg_popstr(incmsg);
+	string op = s;
+	free(s);
+	if(op == "addworker"){
+		addWorker(id, incmsg, outmsg);
+		zmsg_send(&outmsg, client);
+		zframe_destroy(&id);
+		return;		
+	}
+	else return;
+	zframe_destroy(&id);
+	
+}
+
+int main(void) {
+	zctx_t* context = zctx_new();
+	void* server = zsocket_new(context,ZMQ_ROUTER);
+    zsocket_bind(server, "tcp://*:5555");
+    void* client = zsocket_new(context,ZMQ_ROUTER);
+	zsocket_bind(client, "tcp://*:5556");
+	
+	zmsg_t* incmsg;
+	
+	
+	zmq_pollitem_t items[] = {{client, 0, ZMQ_POLLIN, 0}, {server, 0, ZMQ_POLLIN, 0}};
+	cout << "before while" << endl;
+	while(true){
+		zmq_poll(items,2,10*ZMQ_POLL_MSEC);
+    	if(items[0].revents & ZMQ_POLLIN){
+    		zmsg_t* outmsg = zmsg_new();
+    		
+    		incmsg = zmsg_recv(client);
+    		zmsg_print(incmsg);
+    		dispatchWorker(incmsg, outmsg, client, server);
+    		
+    		cout << "Dafuq" << endl;
+    		
+    		zmsg_destroy(&incmsg);
+    		
+			
+			
+    		
+    	}
+    	else if (workers.size() > 0){
+    		this_thread::sleep_for(chrono::seconds(1));
+    		zmsg_t* outmsg = zmsg_new();
+    		zframe_t* w_id = zframe_dup(workers["add"][0]);
+    		zmsg_append(outmsg, &w_id);
+    		zmsg_addstr(outmsg, "Ficty");
+    		zmsg_addstr(outmsg, "op");
+    		zmsg_addstr(outmsg, "3 + 3");
+    		zmsg_print(outmsg);
+    		cout <<"Here's the fault but i don't know why!"<<endl;
+    		zmsg_send(&outmsg, client);		
+    		
+    		
+    		
+    	}
+    	
+    	
+    }
+    
+    
+	zctx_destroy(&context);
+    return 0;
 }
--- OPCS2/slave.cpp
+++ OPCS2/slave.cpp
@@ -3,9 +3,96 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+void op(zmsg_t* incmsg, zmsg_t* outmsg){
+	char* buffer = zmsg_popstr(incmsg);
+	char op;
+	int x, y, ans;
+	sscanf(buffer, "%i %c %i", &x, &op, &y );
+	free(buffer);
+	if(op == '+'){
+		ans = x + y;
+	}
+	else if(op == '-'){
+		ans = x - y;
+	}
+	else if(op == '*'){
+		ans = x*y;
+	}
+	else if(op == '/'){
+		ans = x / y;
+	}
+	else{
+		zmsg_addstr(outmsg, "Not supported");
+		return;
+	}
+	sprintf(buffer, "%d", ans);
+	zmsg_addstr(outmsg, buffer);
+	free(buffer);
+	return;
+}
 
+void dispatch(zmsg_t* incmsg, zmsg_t* outmsg){
+	char* s = zmsg_popstr(incmsg);
+	if(strcmp(s, "op") == 0){
+		op(incmsg, outmsg);
+	}
+	else{
+		zmsg_addstr(outmsg, "error");		
+	}
+	return;
+}
+
+int main(int argc, char** argv){
+	zctx_t* context = zctx_new();
+    void* server = zsocket_new(context,ZMQ_DEALER);
+    zsocket_connect(server, "tcp://localhost:5556");
+    string w_type = "";
+    if(argc > 1){
+    	w_type = argv[1];
+    }
+    else w_type = "add";
+    string serverans = "";
+    zmsg_t* inmsg;
+    zmsg_t* outmsg = zmsg_new();
+    zmsg_addstr(outmsg, "addworker");
+    zmsg_addstr(outmsg, "add");
+    while (serverans != "Thanks"){
+    	zmsg_print(outmsg);
+    	zmsg_send(&outmsg, server);
+    	inmsg = zmsg_recv(server);
+    	zmsg_print(inmsg);
+    	char* s = zmsg_popstr(inmsg);
+    	serverans = s;
+    	free(s);
+    	   	    	
+    }
+    zmsg_destroy(&inmsg);
+	zmsg_destroy(&outmsg);
+	outmsg = zmsg_new();
+	zframe_t* id;
+	zmq_pollitem_t items[] = {{server, 0, ZMQ_POLLIN, 0}};
+	cout << "before while " << endl;
+	while(true){
+		zmq_poll(items,1,10*ZMQ_POLL_MSEC);
+		if(items[0].revents & ZMQ_POLLIN){
+			cout << "on if while " << endl;
+			inmsg = zmsg_recv(server);
+			zmsg_print(inmsg);
+			id = zmsg_pop(inmsg);
+			zmsg_addstr(outmsg, "send");
+			dispatch(inmsg, outmsg);
+			zmsg_append(outmsg, &id);
+			zmsg_send(&outmsg, server);
+			
+			zframe_destroy(&id);
+			zmsg_destroy(&inmsg);
+			
+			outmsg = zmsg_new();
+			
+		}
+		
+	}
     
-    
+    zctx_destroy(&context);
     return 0;
 }
